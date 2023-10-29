1e6f9157db3512ad2afa2508dbc5917f571c7de0
BomScoob <sorawitkraukham@gmail.com>
2023-10-29 19:20:08 +0700

    Remove student sevrlet

--- src/main/java/sit/int202/sitspacemanagement/controller/RemoveStudentServlet.java
+++ src/main/java/sit/int202/sitspacemanagement/controller/RemoveStudentServlet.java
@@ -5,13 +5,23 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.SneakyThrows;
+import sit.int202.sitspacemanagement.models.StudentRepository;
 
 import java.io.IOException;
 
 @WebServlet(name = "RemoveStudentServlet", value = "/remove-student")
 public class RemoveStudentServlet extends HttpServlet {
+    @SneakyThrows
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        HttpSession session = req.getSession();
+        int removeId = Integer.parseInt(req.getParameter("removeId"));
+        StudentRepository studentRepository = (StudentRepository) session.getAttribute("repo");
+        if(studentRepository == null){
+            System.out.println("Can't find student repository");
+        }
+        studentRepository.removeStudentById(removeId);
     }
 }
